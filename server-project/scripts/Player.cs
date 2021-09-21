ba772c1ecb8848aff4040226710653006735b952
graziano <graziano@pop-os.localdomain>
2021-09-21 17:32:21 -0300

    minor improvement

--- server-project/scripts/Player.cs
+++ server-project/scripts/Player.cs
@@ -7,17 +7,29 @@ public class Player : KinematicBody2D
     public int net_id = 0;
 
     private Network network;
+    
+    private byte last_instruction;
+    private Vector2 last_pos_sent;
 
     public override void _Ready()
     {
         network = GetNode("/root/Network") as Network;
     }
 
+    public override void _PhysicsProcess(float delta)
+    {
+        Position += PlayerMovement.Movement(last_instruction, delta);
+    }
+
     public void ProcessMovementRequest(uint tick, byte instruction)
     {
-        float delta = GetPhysicsProcessDeltaTime();
-        Position += PlayerMovement.Movement(instruction, delta);
+        last_instruction = instruction;
+
+        if (last_pos_sent == Position)
+            return;
+
         network.SendUpdatePosition(net_id, tick, Position);
+        last_pos_sent = Position;
     }
 
     public void Setup(int id, string usr, Vector2 pos)
